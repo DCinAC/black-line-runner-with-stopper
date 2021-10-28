19786aa4ba5a724c8f48bb7ddd40dff36a2afb0a
DCinAC <61953469+DCinAC@users.noreply.github.com>
2021-10-29 04:45:13 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 radio.setGroup(69)
 basic.forever(function () {
     if (mbit_Robot.Avoid_Sensor(mbit_Robot.enAvoidState.NOOBSTACLE)) {
-        if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.Black)) {
+        if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.Black) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.White)) {
             mbit_Robot.CarCtrlSpeed2(mbit_Robot.CarState.Car_Run, 20, 50)
-        } else if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.Black)) {
+        } else if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.White) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.Black)) {
             mbit_Robot.CarCtrlSpeed2(mbit_Robot.CarState.Car_Run, 50, 20)
-        } else {
+        } else if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.Black) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.Black)) {
             mbit_Robot.CarCtrlSpeed2(mbit_Robot.CarState.Car_Run, 50, 50)
+        } else {
+        	
         }
     } else {
         mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
